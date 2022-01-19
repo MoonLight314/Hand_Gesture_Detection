23823c1e90d3a3f1036689d734d928f8686d511a
Yi Chang Sic <41887456+MoonLight314@users.noreply.github.com>
2022-01-19 10:37:24 +0900

    Update Train_Rev_02.py

--- Train_Rev_02.py
+++ Train_Rev_02.py
@@ -10,6 +10,8 @@ import numpy as np
 import pandas as pd
 import cv2 as cv
 import os
+import pydevd
+from collections import deque
 
 from sklearn.preprocessing import LabelEncoder 
 from sklearn.model_selection import train_test_split
@@ -25,9 +27,9 @@ from tensorflow.keras.callbacks import TensorBoard , ModelCheckpoint
 #BATCH_SIZE = 4
 BATCH_SIZE = 1
 DROP_OUT_RATE = 0.25
-INPUT_SHAPE = (8, 112, 112, 3)
-
-
+INPUT_SHAPE = ( 8, 112, 112, 3)
+DEST_SIZE = (112 , 112)
+MAX_FRAME = 120
 
 
 meta = pd.read_csv("Meta_Data_220117_Rev_01.csv")
@@ -61,29 +63,68 @@ val_dataset = tf.data.Dataset.from_tensor_slices( (X_test , y_test) )
 
 
 
+
 #
-def generate_data( file_path , label ):
+def Adjust_Length( data ):
+    #pydevd.settrace(suspend=False)
+    # 목적은 ( MAX_FRAME , 112 , 112 , 3) 형태로 만드는 것
+    zero = np.zeros_like( data[0] )
+    d = np.array(data)
+
+    length = d.shape[0]
+
+    for _ in range(int((MAX_FRAME - length)/2)):
+        data.append( zero )
+        data.appendleft( zero )
+
+    if len(data) != MAX_FRAME:
+        data.append( zero )
+
+    aligned_data = np.array(data)
+    
+    return aligned_data
+
+
+
+
+
+#
+def generate_data( file_path , label ):    
+    
+    #pydevd.settrace(suspend=False)
     
     filename = str(file_path.numpy())
-    filename = filename.replace("./Train_Data","C:/Users/csyi/Desktop/Hand_Gesture_Detection/Train_Data")
-    #filename = 'C:/Users/csyi/Desktop/Hand_Gesture_Detection/Train_Data\Shake_Hand\\Video_Test_22.mp4'
-    print(filename[0] , filename[1])
-
+    filename = filename.replace("b'./Train_Data","C:/Users/csyi/Desktop/Hand_Gesture_Detection/Train_Data")
+    filename = filename.replace("'","")
+    
     cap = cv.VideoCapture( filename )
 
-    if cap.isOpened():
-        print("Opened")
-    else:
-        print("Error")
+    # 값들을 255로 나누기
+
+    if cap.isOpened() == False:
+        print("Open Error")
+
+    data = deque([])
+
+    while( True ):
+        ret, frame = cap.read()
+
+        if ret == False:
+            break
+
+        frame = cv.resize( frame, dsize=DEST_SIZE, interpolation=cv.INTER_AREA )
+        
+        data.append( frame )
     
-    data = []
+    data = Adjust_Length( data )
+    data = data / 255
+
     return data , label
 
 
 
 
-#train_dataset = train_dataset.shuffle(buffer_size=len(X_train))\
-train_dataset = train_dataset.shuffle(buffer_size=1)\
+train_dataset = train_dataset.shuffle(buffer_size=len(X_train))\
                 .map(   lambda X_train , y_train:
                         tf.py_function(
                             func = generate_data,
@@ -103,11 +144,13 @@ val_dataset = val_dataset.shuffle(buffer_size=len(X_test))\
 """
 
 
-
-
+"""
 for data, label in train_dataset.take(1):
+#for idx,(data, label) in enumerate(train_dataset):
+    #print( idx, data, label )
     print( data, label )
-
+    #pass
+"""
 
 
 def define_model( input_shape ):
@@ -118,13 +161,20 @@ def define_model( input_shape ):
     model.add(BatchNormalization())
     model.add(MaxPooling3D(pool_size = [1,2,2], strides = [1,2,2], padding = "same"))
 
+
     model.add(Conv3D(128, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform', padding="same"))
     model.add(BatchNormalization())
     model.add(MaxPooling3D(pool_size = [2,2,2], strides = [2,2,2], padding = "same"))
-
+    
     model.add(Conv3D(256, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform', padding="same"))
     model.add(BatchNormalization())
     model.add(Conv3D(256, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform', padding="same"))
+    model.add(BatchNormalization())
+    model.add(MaxPooling3D(pool_size = [2,2,2], strides = [2,2,2], padding = "same"))
+    
+    model.add(Conv3D(512, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform', padding="same"))
+    model.add(BatchNormalization())
+    model.add(Conv3D(512, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform', padding="same"))
     model.add(BatchNormalization())
     model.add(MaxPooling3D(pool_size = [2,2,2], strides = [2,2,2], padding = "same"))
 
@@ -132,13 +182,8 @@ def define_model( input_shape ):
     model.add(BatchNormalization())
     model.add(Conv3D(512, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform', padding="same"))
     model.add(BatchNormalization())
-    model.add(MaxPooling3D(pool_size = [2,2,2], strides = [2,2,2], padding = "same"))
 
-    model.add(Conv3D(512, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform', padding="same"))
-    model.add(BatchNormalization())
-    model.add(Conv3D(512, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform', padding="same"))
-    model.add(BatchNormalization())
-
+    
     model.add( Flatten() )
     model.add( Dense(512, activation='relu') )
     model.add( Dropout(DROP_OUT_RATE) )
@@ -152,6 +197,12 @@ def define_model( input_shape ):
 model = define_model( INPUT_SHAPE )
 print( model.summary() )
 
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(1e-3),
+    loss='categorical_crossentropy',
+    metrics=['categorical_accuracy']
+)
 
 
 model.fit( train_dataset,
